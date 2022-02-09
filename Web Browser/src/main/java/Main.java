7ae0926468e97a84b3c66423288d1c76a70b96b2
Zer0Calib3r <82219229+JfksAssociate@users.noreply.github.com>
2022-02-09 12:15:07 -0500

    Add files via upload

---
+++ Web Browser/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import java.applet.*;
+import java.awt.*;
+
+public class Main extends Applet{
+    public void paint(Graphics g){
+        g.drawString("Welcome in Java Applet.",40,20);
+    }
+    public void showWeb(B)
+}
